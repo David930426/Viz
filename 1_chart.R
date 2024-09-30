c110b2f42111ade5c2046d33b952025d22e791ca
David <davidgunawanwisno@gmail.com>
2024-09-30 16:01:48 +0800

    accident death

--- 1_chart.R
+++ 1_chart.R
@@ -2,9 +2,17 @@
 # David
 # Practice bar chart
 #
+#640 x 480
 
 library(gcookbook)
 library(ggplot2)
+
+gggeom_col()ggplot(pg_mean, aes(x = group, y = weight)) +
+  geom_point()
+
+ggplot(pg_mean, aes (x="", y = weight, fill = group)) +
+  geom_col() +
+  coord_polar(theta="y")
 
 ggplot(pg_mean, aes(x = group, y = weight, fill = group)) + 
   geom_col() +
@@ -18,13 +26,6 @@ ggplot(BOD, aes(x = Time, y = demand)) +
        caption = "By David, Data visualization course, Tunghai University, 2024") +
   theme(plot.title = element_text(hjust = 0.5, size = 20))
 
-gggeom_col()ggplot(pg_mean, aes(x=group, y=weight)) +
-  geom_point()
-
-ggplot(pg_mean, aes (x="", y=weight, fill=group)) +
-  geom_col() +
-  coord_polar(theta="y")
-
 ggplot(diamonds, aes(x = carat)) +
   geom_histogram() +
   labs(title = "Histogram chart", 
@@ -37,4 +38,10 @@ ggplot(diamonds, aes(x = carat)) +
        caption = "By David, Data visualization course, Tunghai University, 2024") +
   theme(plot.title = element_text(hjust = 0.5, size = 20))
 
-#640 x 480
+df <- read.csv('Accidents.csv')
+
+ggplot(df, aes(x = Year, y = Death)) +
+  geom_col() +
+  labs(title = "Bar chart", 
+       caption = "By David, Data visualization course, Tunghai University, 2024") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
