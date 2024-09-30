64c75994ba495283c020e71d2d8fb841651a90f9
David <davidgunawanwisno@gmail.com>
2024-09-30 14:41:20 +0800

    update

--- 1_chart.R
+++ 1_chart.R
@@ -8,3 +8,10 @@ library(ggplot2)
 
 ggplot(pg_mean, aes(x=group, y=weight)) + 
   geom_col()
+
+ggplot(pg_mean, aes(x=group, y=weight)) +
+  geom_point()
+
+ggplot(pg_mean, aes (x="", y=weight, fill=group)) +
+  geom_col() +
+  coord_polar(theta="y")
