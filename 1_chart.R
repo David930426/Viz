026fc1f6d8ff8dd9de401929bceaa1118c7e9eaa
David <davidgunawanwisno@gmail.com>
2024-09-30 15:29:15 +0800

    perubahan di bagian line

--- 1_chart.R
+++ 1_chart.R
@@ -12,11 +12,17 @@ ggplot(pg_mean, aes(x = group, y = weight, fill = group)) +
        caption = "By David, Data visualization course, Tunghai University, 2024") +
   theme(plot.title = element_text(hjust = 0.5, size = 20))
 
-#ggplot(pg_mean, aes(x=group, y=weight)) +
-  #geom_point()
+ggplot(BOD, aes(x = Time, y = demand)) +
+  geom_line() +
+  labs(title = "Line chart", 
+       caption = "By David, Data visualization course, Tunghai University, 2024") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
 
-#ggplot(pg_mean, aes (x="", y=weight, fill=group)) +
-  #geom_col() +
-  #coord_polar(theta="y")
+ggplot(pg_mean, aes(x=group, y=weight)) +
+  geom_point()
+
+ggplot(pg_mean, aes (x="", y=weight, fill=group)) +
+  geom_col() +
+  coord_polar(theta="y")
 
 #640 x 480
