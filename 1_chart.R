2334f2a26001ca98316b68bad8cf80c4e9235329
David <davidgunawanwisno@gmail.com>
2024-09-30 16:03:47 +0800

    naice

--- 1_chart.R
+++ 1_chart.R
@@ -17,31 +17,33 @@ ggplot(pg_mean, aes (x="", y = weight, fill = group)) +
 ggplot(pg_mean, aes(x = group, y = weight, fill = group)) + 
   geom_col() +
   labs(title = "Bar chart", 
-       caption = "By David, Data visualization course, Tunghai University, 2024") +
+       caption = "By David, Data Visualization Course, Tunghai University, 2024") +
   theme(plot.title = element_text(hjust = 0.5, size = 20))
 
 ggplot(BOD, aes(x = Time, y = demand)) +
   geom_line() +
   labs(title = "Line chart", 
-       caption = "By David, Data visualization course, Tunghai University, 2024") +
+       caption = "By David, Data Visualization Course, Tunghai University, 2024") +
   theme(plot.title = element_text(hjust = 0.5, size = 20))
 
 ggplot(diamonds, aes(x = carat)) +
   geom_histogram() +
   labs(title = "Histogram chart", 
-       caption = "By David, Data visualization course, Tunghai University, 2024") +
+       caption = "By David, Data Visualization Course, Tunghai University, 2024") +
   theme(plot.title = element_text(hjust = 0.5, size = 20))
 
 ggplot(diamonds, aes(x = carat)) +
   geom_bar() +
   labs(title = "Bar chart", 
-       caption = "By David, Data visualization course, Tunghai University, 2024") +
+       caption = "By David, Data Visualization Course, Tunghai University, 2024") +
   theme(plot.title = element_text(hjust = 0.5, size = 20))
 
 df <- read.csv('Accidents.csv')
 
 ggplot(df, aes(x = Year, y = Death)) +
   geom_col() +
-  labs(title = "Bar chart", 
-       caption = "By David, Data visualization course, Tunghai University, 2024") +
+  labs(title = "Accidents: Death", 
+       caption = "By David, Data Visualization Course, Tunghai University, 2024") +
   theme(plot.title = element_text(hjust = 0.5, size = 20))
+
+
