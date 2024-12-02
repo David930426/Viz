5a0f7131582197248aabc771a32f469d8d1baf33
David <davidgunawanwisno@gmail.com>
2024-12-02 14:29:34 +0800

    new

--- axes.R
+++ axes.R
@@ -29,14 +29,35 @@ p1 <- ggplot(PlantGrowth, aes(x = group, y = weight)) +
   geom_boxplot() +
   labs(title = "Group VS Weight",
        caption = "By David, DV, THU, 2024") +
-  theme(plot.title = element_text(hjust = 0.5, size = 20))
+  theme(plot.title = element_text(hjust = 0.5, size = 20)) +
+  ylim(0, max(PlantGrowth$weight))
 
 p2 <- ggplot(PlantGrowth, aes(x = group, y = weight)) +
   geom_boxplot() +
   coord_flip()+ 
   labs(title = "Weight VS Group",
        caption = "By David, DV, THU, 2024") +
-  theme(plot.title = element_text(hjust = 0.5, size = 20))
+  theme(plot.title = element_text(hjust = 0.5, size = 20)) +
+  ylim(0, max(PlantGrowth$weight))
 
 grid.arrange(p1, p2, ncol = 2,
-             top = 'Arrange two plots side by side')
+             top = 'Setting the Range of a Continuous Axis')
+
+library(ggplot2)
+library(gridExtra)
+
+p1 <- ggplot(PlantGrowth, aes(x = group, y = weight)) +
+  geom_boxplot() +
+  labs(title = "Group VS Weight",
+       caption = "By David, DV, THU, 2024") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
+
+p2 <- ggplot(PlantGrowth, aes(x = group, y = weight)) +
+  geom_boxplot() +
+  labs(title = "Group VS Weight",
+       caption = "By David, DV, THU, 2024") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20)) +
+  ylim(0, max(PlantGrowth$weight))
+
+grid.arrange(p1, p2, ncol = 2,
+             top = 'Setting the Range of a Continuous Axis')
